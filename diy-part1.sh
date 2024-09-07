c487232aa2e9c24653f2b785f4393e74de0c7732
qzyuebing <54806519+qzyuebing@users.noreply.github.com>
2024-09-07 09:28:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,7 @@ sed -i '$a src-git Aibx https://github.com/Aibx/openwrt-packages' feeds.conf.def
 sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.conf.default
 sed -i '$a src-git mmdvm https://github.com/qzyuebing/mmdvm-openwrt' feeds.conf.default
 #sed -i '$a src-git mmdvm https://github.com/lazywalker/mmdvm-openwrt' feeds.conf.default
-sed -i '$a src-git devtools https://github.com/lazywalker/devtools-feeds' feeds.conf.default
+#sed -i '$a src-git devtools https://github.com/lazywalker/devtools-feeds' feeds.conf.default
 sed -i '$a src-git pptp https://github.com/qzyuebing/luci-app-pptp-server' feeds.conf.default
 
 
